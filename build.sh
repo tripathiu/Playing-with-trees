a4997d81ee0da486dcc5ff4150be17432bc5e673
Tripathi <tri.utkarsh7596@gmail.com>
2020-07-16 22:15:29 +0530

    Fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-g++ -I$(dirname $0)/inc $(dirname $0)/main.cpp $(dirname $0)/element.cpp -o $(dirname $0)/main
-$(dirname $0)/main
+g++ -I$(dirname $0)/inc $(dirname $0)/main.cpp $(dirname $0)/element.cpp -o $(dirname $0)/main && $(dirname $0)/main
